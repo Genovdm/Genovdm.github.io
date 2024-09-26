85b28a10b60d42a0e9709ff3de5b0da019fc915c
Genovdm <website487@gmail.com>
2024-09-26 12:46:26 +0200

    Added website files

---
+++ script.js
@@ -0,0 +1,6 @@
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'CV/EUGENE_VAN_DER_MERWE_CV.pdf'; // Path to your CV file
+    link.download = 'EUGENE_VAN_DER_MERWE_CV.pdf'; // Filename to be downloaded
+    link.click();
+}
